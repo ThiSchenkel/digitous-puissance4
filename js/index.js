932836fb93e5e1fbb55b0bc670e8c0af8d81032e
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-19 11:50:11 +0100

    The beginning

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
+
+var board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+];
+
 $(document).ready(function () {
-    var board = [
-        [0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0]
-        [0, 0, 0, 0, 0, 0, 0]
-    ];
-
     function printBoard() {
-
-        var boardHtml = `<div class="container">`;
+        var boardHtml = `<div id="container">`;
         for (var i = 0; i < 6; i++) {
             boardHtml += `<div class="row" id="row${i}">`;
             for (var j = 0; j < 7; j++) {
-                boardHtml += `<div class="col-1 case" id="column${j}">${board[i][j]}</div>`;
+                boardHtml += `<div id = "col${j}" class="col-1 case">${board[i][j]}</div>`;
             }
             boardHtml += `</div>`;
         }

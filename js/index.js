74b9a53785d5c5ecadf0f1faa274da273c202aed
Nerfic <jdll.allan@gmail.com>
2021-01-21 13:43:03 +0100

    add function checkColumn for win

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
             $(`#${line[column]}-${column}`).addClass(pionPlayer) // Alors j'ajoute un pion
             board[line[column]][column] = player; // Remplace par le chiffre du joueur
             checkRow(line[column], player);
-            // checkColumn(line[column], player);
+            checkColumn(column, player);
         } else if (board[line[column]][column] === 1 || board[line[column]][column] === 2) { // Si non si la ligne = 1
             if (line[column] === 0) { // Si le joueur essaye de jouer sur la première ligne
                 console.log("fin de la colonne, impossible de rajoute des pions"); // Alors j'indique qu'on ne peut plus mettre de pion
@@ -57,33 +57,30 @@ $(document).ready(function () {
                 $(`#${line[column]}-${column}`).addClass(pionPlayer) // Je joue le pion
                 board[line[column]][column] = player; // Remplace par le chiffre du joueur
                 checkRow(line[column], player);
-                // checkColumn(line[column], player);
+                checkColumn(column, player);
             }
         }
         choosePlayer();
     }
 
-    function checkRow(row, player) {
-        for (var i = 0; i < 7; i++) {
-            if (board[row][i] === player &&
-                board[row][i + 1] === player &&
-                board[row][i + 2] === player &&
-                board[row][i + 3] === player) {
-                console.log("Gagné! en ligne par le joueurs", player);
+    function checkRow(row, player) { // Fonction win horizontale
+        for (var i = 0; i < 4; i++) {
+            if (board[row][i] === player && // Check si a la position "i" il y a le p1
+                board[row][i + 1] === player && // Check si a la position "i + 1" il y a le p1
+                board[row][i + 2] === player && // Check si a la position "i + 2" il y a le p1
+                board[row][i + 3] === player) { // Check si a la position "i + 3" il y a le p1
+                console.log(`Gagné! en ligne par le joueurs ${player} sur la ligne ${row}`);
             }
         }
     }
 
-    function checkColumn(column, player) {
-        for (var i = 0; i < 6; i++) {
+    function checkColumn(column, player) { // Fonction win verticale
+        for (var i = 0; i < 3; i++) {
             if (board[i][column] === player &&
                 board[i + 1][column] === player &&
                 board[i + 2][column] === player &&
                 board[i + 3][column] === player) {
-                console.log("Gagné! en colonne par le joueurs", player)
-                break;
-            } else {
-                console.log("Pas de win colonne")
+                console.log(`Gagné! en ligne par le joueurs ${player} sur la colonne ${column}`)
             }
         }
     }

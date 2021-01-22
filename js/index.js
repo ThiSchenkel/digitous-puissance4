7ddab36afb39a82217d9cb8ee1369ae88b8f90b8
unknown <yestsering@gmail.com>
2021-01-22 13:55:51 +0100

    responsive

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             boardHtml += `</div>`;
         }
         boardHtml += `</div>`;
-        boardHtml += `<div class="row centercol">`;
+        boardHtml += `<div class="row centercol ">`;
         for (var k = 0; k < 7; k++) { // Génère les button
             boardHtml += `<div class="col-1"><input type="button" class="buttoncol " value="${k + 1}" id="column${k}" alt="play at ${k + 1}"></div>`;
         }
